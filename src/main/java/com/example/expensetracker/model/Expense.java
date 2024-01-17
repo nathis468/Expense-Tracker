16f9d271b2b29185d11622a0c2cb3fee04ff2524
nathis468 <nathis468@gmail.com>
2024-01-17 20:39:49 +0530

    crud1

---
+++ src/main/java/com/example/expensetracker/model/Expense.java
@@ -0,0 +1,34 @@
+package com.example.expensetracker.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Document(collection = "expenses")
+@Data
+@AllArgsConstructor
+    public class Expense {
+    @Id
+    private String id;
+
+    @Field("user_id")
+    @DocumentReference(collection = "users")
+    private User    userId;
+
+    @Field("data_time")
+    private LocalDateTime dateAndTime;
+    private String description;
+    private BigDecimal amount;
+    
+    public Expense() {
+        this.dateAndTime = LocalDateTime.now();
+    }
+}
